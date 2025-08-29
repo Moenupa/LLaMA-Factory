9d0660cf497f18d7a8993343f0be64de28cb1d54
Moenupa <meng.wang@cair-cas.org.hk>
2025-08-29 11:43:30 +0800

    fix: better infer slurm script

--- slurm/infer_lora.sh
+++ slurm/infer_lora.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
-#SBATCH -o logs/lora-infer/%j.out
-#SBATCH -e logs/lora-infer/%j.err
-#SBATCH -J Qwen3-lora-8b-infer-sentencing
-#SBATCH --partition=a100
 #SBATCH --nodes=1
+#SBATCH --mem-per-gpu=64G
+#SBATCH --cpus-per-gpu=16
+#SBATCH -p a100
+#SBATCH -t 30-00:00:00
+#SBATCH -o logs/legal-lora-infer/%j.out
+#SBATCH -e logs/legal-lora-infer/%j.err
+#SBATCH -J Legal-Qwen3-8B-LoRA-Infer
+#SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem=64GB
-#SBATCH --time=7-00:00:00
+#SBATCH --array=0
 
-INFER_CONFIG=examples/inference/qwen3_rag_eval.yaml
+# major settings
+SAVE_PATH="saves/qwen3-8b"
+MODEL="$SAVE_PATH/lora-16-pk/sft"
+datasets=("sentencing_math12k_test" "sentencing_pk_math12k_test")
 
+# get proper params
+INFER_CONFIG=examples/inference/qwen3_lora_eval.yaml
 echo "GPUs assigned by Slurm: $CUDA_VISIBLE_DEVICES, $INFER_CONFIG"
-cat $INFER_CONFIG
 
-echo "------------------------------------------------------------------------------"
+set -x
 
-llamafactory-cli train $INFER_CONFIG
+MODEL_BASENAME=$(basename $MODEL)
+MODEL_ALIAS=$(basename $(dirname $MODEL))
+DATASET=${datasets[$SLURM_ARRAY_TASK_ID]}
+WANDB_DISABLED="true" llamafactory-cli train $INFER_CONFIG \
+    adapter_name_or_path=$MODEL \
+    eval_dataset=$DATASET \
+    output_dir=$SAVE_PATH/$MODEL_ALIAS/$DATASET
